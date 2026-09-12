b7b3b9177a6c7c1227313dfb74d93854791f6819
pokji18 <dragonxxxx92@gmail.com>
2026-09-12 07:15:51 +0000

    Install SusFS v2.2.0 backport + enable in defconfig
    Cherry-picked KernelSU SusFS v2.2.0 backport (los sm6150
    912522b8ce13, adapted to 4.14.369 monolithic smaps design):
    - new fs/susfs.c, include/linux/susfs.h + susfs_def.h
    - hooks in fs/*, mm/memory.c, kernel/{sys,kallsyms}.c, mount/sched
      headers, selinux/avc.c (all CONFIG_KSU_SUSFS-guarded)
    - kept local FAKE_UNAME block in kernel/sys.c, added susfs spoof call
    - task_mmu: susfs checks folded into 3-arg show_smap (dropped
      duplicate 2-arg wrapper + split-design show_smaps_rollup)
    - both defconfigs: KPROBES=y, KSU_SUSFS=y + full sub-options
    - 19 objects incl. ksu.o compile clean (clang 22)

--- fs/exec.c
+++ fs/exec.c
@@ -1726,7 +1726,6 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
-
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
 	 * set*uid() to execve() because too many poorly written programs
@@ -1902,6 +1901,7 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1929,6 +1929,7 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
