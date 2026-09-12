e35c6ff9b279eb0ff1a34c5e2aca30d2f43b45a5
pokji18 <dragonxxxx92@gmail.com>
2026-09-12 04:45:33 +0000

    Boeffla: drop qcom_rx_wakelock from default block list
    - Modem RX wakelock is needed for paging: blocking it breaks
      VoLTE call reception and SMS delivery
    - Keep only WiFi-scan + NETLINK entries

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT			"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;NETLINK"
+#define LIST_WL_DEFAULT			"wlan;wlan_wow_wl;wlan_extscan_wl;NETLINK"
 
 #define LENGTH_LIST_WL			1024
 #define LENGTH_LIST_WL_DEFAULT		1024
